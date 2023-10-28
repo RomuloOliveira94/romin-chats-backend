936d57b7c56d0c7bf9aeaa4cd8aae7e65936889f
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-10-27 22:13:10 -0300

    #initial setup

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const app = express();
+
+app.use(cors());
+
+
+const server_port = 3001;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  },
+});
+
+io.on("connection", (socket) => {
+
+  function getQuantityOfUsers (roomID) {
+    const room = io.sockets.adapter.rooms.get(roomID);
+    const numUsers = room ? room.size : 0;
+    return socket.to(roomID).emit("userQuantity", numUsers)
+  }
+
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`User desconnected: ${socket.id}`);
+  });
+
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+    socket.to(data.room).emit("receive_message", data)
+    getQuantityOfUsers(data.room)
+    console.log(`User: ${socket.id}, entrou na sala ${data.room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data)
+  });
+
+  socket.on("leaveRoom", (data) => {
+    socket.leave(data.room);
+    console.log(`User: ${socket.id}, saiu da sala ${data.room}`);
+    socket.to(data.room).emit("receive_message", data);
+    getQuantityOfUsers(data.room)
+  });
+});
+
+server.listen(server_port, () => {
+  console.log(`Server rodando em localhost:${server_port}`);
+})
+
+
+

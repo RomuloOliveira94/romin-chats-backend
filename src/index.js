1249382efc378fecbd9204f24db993bab16ceefc
Rômulo Augusto <romuloffall@gmail.com>
2024-04-18 20:03:35 -0300

    #add node modules

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-const app = express();
-const router = require('./router');
+const router = require("./router");
 const { Server } = require("socket.io");
-
-app.use(cors({
-	origin: 'https://romin-chats.vercel.app/'
-}));
-
+const app = express();
+app.use(cors(), {
+  origin: "http://localhost:5173/",
+  credentials: true,
+});
 app.use(router);
-
+aap.set("port", process.env.PORT || 5000);
 
 const server_port = 5000;
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://romin-chats.vercel.app/"
-  }
+    origin: "*",
+  },
 });
 
-
 io.on("connection", (socket) => {
-
-  function getQuantityOfUsers (roomID) {
+  function getQuantityOfUsers(roomID) {
     const room = io.sockets.adapter.rooms.get(roomID);
     const numUsers = room ? room.size : 0;
-    return socket.to(roomID).emit("userQuantity", numUsers)
+    return socket.to(roomID).emit("userQuantity", numUsers);
   }
 
   console.log(`User Connected: ${socket.id}`);
@@ -38,26 +35,23 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => {
     socket.join(data.room);
-    socket.to(data.room).emit("receive_message", data)
-    getQuantityOfUsers(data.room)
+    socket.to(data.room).emit("receive_message", data);
+    getQuantityOfUsers(data.room);
     console.log(`User: ${socket.id}, entrou na sala ${data.room}`);
   });
 
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data)
+    socket.to(data.room).emit("receive_message", data);
   });
 
   socket.on("leaveRoom", (data) => {
     socket.leave(data.room);
     console.log(`User: ${socket.id}, saiu da sala ${data.room}`);
     socket.to(data.room).emit("receive_message", data);
-    getQuantityOfUsers(data.room)
+    getQuantityOfUsers(data.room);
   });
 });
 
 server.listen(server_port, () => {
   console.log(`Server rodando em localhost:${server_port}`);
-})
-
-
-
+});
